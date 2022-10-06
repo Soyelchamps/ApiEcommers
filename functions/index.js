5fc2a908ce317a06e8fac6c307ca1b636922aff1
Carlos Diaz <92885752+Soyelchamps@users.noreply.github.com>
2022-10-05 21:27:26 -0500

    Configuramos para netilfy

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
-const router = require("./routes");
+const router = require("../routes");
+const serverless = require("serverless-http");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(cors({}));
 app.use(router);
 
 app.listen(3000, (error) => {
   error ? console.log(error) : console.log("Server Running");
 });
+
+module.exports.handler = serverless(app);
