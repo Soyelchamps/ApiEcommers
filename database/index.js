9b0fc6294f9d0e790ef8aef5d184d1502a36c1e2
Carlos Diaz <92885752+Soyelchamps@users.noreply.github.com>
2022-10-06 17:50:40 -0500

    Configuracion 6-octubre- se revisa

--- database/index.js
+++ database/index.js
@@ -1,9 +1,17 @@
 const { Pool } = require("pg");
+// const connect = new Pool({
+//   host: "babar.db.elephantsql.com",
+//   user: "ehgcqzkm",
+//   password: "5HpB910slyGHAhQHa7xnaWFHlOPGbUWd",
+//   database: "ehgcqzkm",
+//   port: "5430",
+//});
+
 const connect = new Pool({
   host: "localhost",
   user: "postgres",
   password: "1234",
-  database: "ecommers",
+  database: "postgres",
   port: "5430",
 });
 
