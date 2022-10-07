d62443d874768f66e32c54fc2a2e9737b0616df6
Carlos Diaz <92885752+Soyelchamps@users.noreply.github.com>
2022-10-06 19:08:46 -0500

    Proyecto final

--- database/index.js
+++ database/index.js
@@ -1,18 +1,18 @@
 const { Pool } = require("pg");
-// const connect = new Pool({
-//   host: "babar.db.elephantsql.com",
-//   user: "ehgcqzkm",
-//   password: "5HpB910slyGHAhQHa7xnaWFHlOPGbUWd",
-//   database: "ehgcqzkm",
-//   port: "5430",
-//});
-
 const connect = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "1234",
-  database: "postgres",
+  host: "babar.db.elephantsql.com",
+  user: "ehgcqzkm",
+  password: "5HpB910slyGHAhQHa7xnaWFHlOPGbUWd",
+  database: "ehgcqzkm",
   port: "5430",
 });
+
+// const connect = new Pool({
+//   host: "localhost",
+//   user: "postgres",
+//   password: "1234",
+//   database: "postgres",
+//   port: "5430",
+// });
 
 module.exports = connect;
